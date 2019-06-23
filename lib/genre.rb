125a357398b578d31a431e5141a5da4e5616d68f
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-23 19:57:13 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,16 +1,20 @@
-class Genre
-	attr_accessor :name
-    @@all = []
- 	def initialize(name)
-		@name = name
-   	@@all << self
-	end
-	
-	def self.all 
-    @@all
+Class Genre
+  attr_accessor :name, :songs
+
+   def initialize(name)
+      @name = name
+      @songs = []
   end
-	  
 
+   def add_song(song)
+      @songs << song
+  end
+
+   def songs
+      @songs
+  end
+
+   def artists
+      @songs.collect { |song| song.artist }
+  end
 end
-
-	
