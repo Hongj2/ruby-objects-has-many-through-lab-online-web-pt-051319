47ed64b030b3643e95fe6342be3f73774faec28c
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-23 19:40:04 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Patient
   attr_accessor :name
   @@all = []
@@ -27,36 +27,5 @@ class Patient
       date.doctor
     end
   end
-  
-=======
-class Patient
-  attr_accessor :name
-  @@all = []
-  
-  def initialize (name)
-    @name = name 
-    @@all << self 
-  end 
-  
-  def self.all
-    @@all 
-  end
-  
-  def new_appointments(date,doctor)
-    Appointment.new(date,self,doctor)
-  end
-  
-  def appointments 
-    Appointment.all.select do |date|
-      date.patient == self
-    end
-  end
-  
-  def doctors
-    date.map do |date|
-      date.doctor
-    end
-  end
-  
->>>>>>> 8cdfa9cee39efe43298d1a7d8ea640dac0b0d0ca
+ 
 end
